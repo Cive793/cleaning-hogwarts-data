dee5efbe27e948da8b23f0a58ad7462658ae08bf
peterstastny <peter.stastny7@gmail.com>
2021-09-07 00:11:50 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", loadJson);
+
+const allStudents = [];
+let studentObj = {
+  firstName: "",
+  lastName: "",
+  middleName: "",
+  mickName: "",
+  photo: "",
+  house: "",
+};
+
+loadJson() {
+
+}
